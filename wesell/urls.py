eac7a09797f12daec5c4402b3aef67d9c4c592aa
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-18 13:49:46 +0200

    Final commit

--- wesell/urls.py
+++ wesell/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('reset_password_complete/',
         auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_done.html'),
         name= "password_reset_complete")
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
