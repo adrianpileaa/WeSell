48808e78e51fb693a0bb2bd61f8201161ec54cd7
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-18 15:46:17 +0200

    Final commit

--- wesell/settings.py
+++ wesell/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p3@ynh*^@+14)r+2&gje))qx7e(o^u$c*-d7z75c#!q)%3lnp#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wesell-crm.herokuapp.com', 'localhost']
 
