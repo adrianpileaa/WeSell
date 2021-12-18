327455938d2887e49919dd1259bf8e4e66d81692
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-18 13:31:54 +0200

    Final commit

--- wesell/settings.py
+++ wesell/settings.py
@@ -88,9 +88,12 @@ DATABASES = {
         'USER' : 'postgres',
         'PASSWORD' : 'banderas231',
         'HOST' : 'localhost',
-        'PORT' : '5432' 
+        'PORT' : '' 
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
