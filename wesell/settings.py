e1d2dd4ec94cd7a5a5048cfd4bee52cddc95c0fb
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-19 12:08:25 +0200

    Final commit

--- wesell/settings.py
+++ wesell/settings.py
@@ -133,14 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+    
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
