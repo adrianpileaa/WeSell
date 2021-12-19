ae66ab5f5e2bdf67a73dffdeb11d9e73ffc699ce
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-19 22:24:54 +0200

    commit

--- wesell/settings.py
+++ wesell/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
