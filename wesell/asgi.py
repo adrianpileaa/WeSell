71cec716385ccf0a05b9db7bbba1394bad2a7980
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-25 11:24:23 +0200

    Commit

--- wesell/asgi.py
+++ wesell/asgi.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesell.settings')
-
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
