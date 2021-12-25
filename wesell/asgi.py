1fc91eb31f1cf5002949e0844391ce4b0863dbcc
adrianpileaa <90518975+adrianpileaa@users.noreply.github.com>
2021-12-25 13:33:25 +0200

    Commit

--- wesell/asgi.py
+++ wesell/asgi.py
@@ -1,17 +1,14 @@
-
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
-from channels.auth import AuthMiddlewareStack
-import products.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesell.settings')
-django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
+from channels.auth import AuthMiddlewareStack
+import products.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -21,16 +18,5 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-'''
 
-import os
-import django
-from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesell.settings')
-
-django.setup()
-
-application = get_default_application()
-
-'''
